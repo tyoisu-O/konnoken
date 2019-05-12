c907abf7da632840842a7b3ee22aada6ff3c3a7e
tyoisu <unban5963@gmail.com>
2019-05-13 08:25:23 +0900

    add css & top.php's php

--- answer.php
+++ answer.php
@@ -1,9 +1,13 @@
 <?php
+$number = $_POST["example"];
+$answer = $_POST["ans"];
+$btn = $_POST["btn"];
+
 
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ja">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=, initial-scale=1.0">
@@ -17,8 +21,13 @@
             <h2>今野検 解答フォーム</h2>
         </header>
         <div class="main">
-            <input type="text" class="answer">
-            <input type="submit" class="answer_btn" value="送信">
+            <select name="example">
+                <option value="1班">1班</option>
+                <option value="2班">2班</option>
+                <option value="3班">3班</option>
+            </select>
+            <input type="text" class="answer" name="ans">
+            <input type="submit" class="answer_btn" value="送信" name="btn">
         </div>
         <footer>
 
