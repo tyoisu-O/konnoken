1d8a8447dcd5d08d823a21eb5943c183d9c5b020
tyoisu <unban5963@gmail.com>
2019-05-12 11:13:06 +0900

    first commit

---
+++ answer.php
@@ -0,0 +1,28 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" type="text/css" href="konnoken.css">
+    <title>今野検 解答フォーム</title>
+</head>
+<body>
+    <form action="answer.php" method="post">
+        <header>
+
+        </header>
+        <div class="main">
+            <input type="text" class="answer">
+            <input type="submit" class="answer_btn" value="送信">
+        </div>
+        <footer>
+
+        </footer>
+        </form>
+</body>
+</html>
